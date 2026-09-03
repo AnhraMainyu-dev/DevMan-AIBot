a21d27a6f2287033b5f860a1f8bf8e58f85f743d
AnhraMainyu <maximheilelov@gmail.com>
2026-09-03 15:34:14 +0300

    setup DialogFlow

---
+++ dialogflow.py
@@ -0,0 +1,17 @@
+from decouple import config
+from google.cloud import dialogflow
+
+GOOGLE_APPLICATION_CREDENTIALS=config("GOOGLE_APPLICATION_CREDENTIALS")
+
+def detect_intent_text(project_id, session_id, text, language_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={'session': session, 'query_input': query_input}
+    )
+    return response.query_result
--- handlers.py
+++ handlers.py
@@ -1,7 +1,10 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.error import Forbidden
 from telegram.ext import ContextTypes, ConversationHandler, CallbackContext
+from dialogflow import detect_intent_text
+from decouple import config
 
+DIALOGFLOW_PROJECT_ID=config("DIALOGFLOW_PROJECT_ID")
 
 async def start(update: Update, context: CallbackContext):
     await update.message.reply_text('Здравствуйте')
@@ -9,3 +12,8 @@ async def start(update: Update, context: CallbackContext):
 async def repeat_after(update: Update, context: CallbackContext):
     text = update.message.text.strip()
     await update.message.reply_text(text)
+
+async def reply(update: Update, context: CallbackContext):
+    text = update.message.text.strip()
+    answer = detect_intent_text(DIALOGFLOW_PROJECT_ID, str(update.message.from_user.id), text, language_code='ru')
+    await update.message.reply_text(answer.fulfillment_text)
