0c9052ffa8a5158a0962af8040f982d53bad6e78
AnhraMainyu <maximheilelov@gmail.com>
2026-09-03 19:09:45 +0300

    add DialogFlow to VK bot

--- send_to_vk.py
+++ send_to_vk.py
@@ -1,17 +1,27 @@
-import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
+from dialogflow import detect_intent_text
 from decouple import config
+import vk_api as vk
+from vk_api.longpoll import VkLongPoll, VkEventType
+import random
 
-VK_API = config('VK_API')
-vk_session = vk_api.VkApi(token=VK_API)
+def send_to_vk(event, vk_api, message):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=message,
+        random_id=random.randint(1,1000)
+    )
 
-longpoll = VkLongPoll(vk_session)
 
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        print('Новое сообщение:')
-        if event.to_me:
-            print('Для меня от: ', event.user_id)
-        else:
-            print('От меня для: ', event.user_id)
-        print('Текст:', event.text)
+def main():
+    dialogflow_project_id = config("DIALOGFLOW_PROJECT_ID")
+    vk_token = config('VK_API')
+    vk_session = vk.VkApi(token=vk_token)
+    vk_api = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            answer = detect_intent_text(dialogflow_project_id, str(event.user_id), event.text, language_code='ru')
+            send_to_vk(event, vk_api, answer.fulfillment_text)
+
+if __name__ == "__main__":
+    main()
